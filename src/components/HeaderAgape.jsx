0f60f6a4e81b32d9c1075bb206383974035c89ba
Mucca03 <alejito4d@gmail.com>
2025-12-05 12:15:11 -0500

    refactor layout: switch grid to absolute/relative + font adjustments and minor fixes

--- src/components/HeaderAgape.jsx
+++ src/components/HeaderAgape.jsx
@@ -2,7 +2,7 @@ import flowersHeader from "../assets/headerAgape.png";
 
 const HeaderAgape = () => {
     return (
-        <nav className="bg-agapeHeader border-gray-200 px-4 lg:px-6 py-2.5">
+        <nav className="bg-agapeHeader border-gray-200 px-4 lg:px-6 py-2.5 font-poppins">
             <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                 <a className="flex items-center">
                     <img src={flowersHeader} className="h-10 sm:h-17" alt="Flowers"/>
@@ -12,9 +12,9 @@ const HeaderAgape = () => {
                         <img src={flowersHeader} className="h-12 sm:h-17" alt="Flowers"/>
                     </a>
                 </div>
-                <div className="bg-white rounded-full inset-shadow-sm inset-shadow-neutral-400 px-7 py-3 text-xl hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+                <div className="bg-white font-normal rounded-full inset-shadow-sm inset-shadow-neutral-400 px-7 py-3 text-xl hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
                      id="mobile-menu-2">
-                    <ul className="flex flex-col mt-4 font-[400] lg:flex-row lg:space-x-24 lg:mt-0">
+                    <ul className="flex flex-col mt-4 lg:flex-row lg:space-x-24 lg:mt-0">
                         <li>
                             <a href="#"
                                className="text-ColorAgape hover:text-HoverAgape">
--- src/components/HeroAgape.jsx
+++ src/components/HeroAgape.jsx
@@ -1,40 +1,51 @@
 import logoAgape from "../assets/logoAgape.png";
-import tortaAgape from "../assets/tortaAgape.png"
+import tortaAgape from "../assets/tortaAgape.png";
 
 const HeroAgape = () => {
     return (
-        <section className="bg-agapeHeader">
-            <div className="grid lg:py-2 lg:px-10 grid-cols-6 grid-rows-3">
-                <div className="relative rounded-full col-start-2 col-span-4">
-                    <img        src={`${logoAgape}`}
-                                className="absolute z-1 top-4 w-120 h-auto left-1/5"
-                                alt="logo"
-                    />
-                    <img        src={`${tortaAgape}`}
-                                className="absolute lg:left-10 lg:w-180 h-auto drop-shadow-cake z-0"
-                                alt="mockup"
-                    />
+        <section className="bg-agapeHeader py-10">
+            <div className="relative max-w-screen-xl mx-auto h-[500px] font-poppins">
+                <img
+                    src={logoAgape}
+                    className="absolute z-10 top-[0px] left-[380px] w-[450px] h-auto"
+                    alt="logo"
+                />
+                <img
+                    src={tortaAgape}
+                    className="absolute z-0 top-[50px] left-[350px] w-[550px] h-auto drop-shadow-cake"
+                    alt="mockup"
+                />
+                <div
+                    className="absolute top-[194px] left-[330px]
+                     text-2xl font-bold text-ColorAgape"
+                >
+                    Pastel Velvet
                 </div>
-
-                <div className="relative rounded-full col-start-2 row-start-2 py-10 z-2">
-                    <h2 className=" text-2xl font-bold px-5 text-ColorAgape">Pastel Velvet</h2>
-                    <p className="text-sm text-ColorAgape
-                       backdrop-blur rounded-full py-2
-                       text-center w-[200px]">
-                        Bizcocho esponjoso de cacao con crema de queso suave y un sello rojo irresistible
-                    </p>
+                <div
+                    className="absolute top-[221px] left-[300px]
+                     text-sm text-ColorAgape
+                     backdrop-blur rounded-full px-[10px] py-[12px]
+                     text-center w-[230px] font-extralight italic"
+                >
+                    Bizcocho esponjoso de cacao con crema de queso suave y un sello rojo irresistible
                 </div>
-                <div className="relative rounded-full col-start-4 px-35 row-start-3 z-2">
-                    <h1 className=" text-2xl font-bold px-13 text-ColorAgape">12.500</h1>
-                    <div className="absolute
-                        cursor-pointer rounded-full border-2 border-colorAgape
-                         backdrop-blur px-8 py-5 font-bold text-lg
-                         text-ColorAgape text-center whitespace-nowrap">
-                        Ordena ahora
-                    </div>
+                <div
+                    className="absolute top-[365px] left-[765px]
+                     text-2xl font-bold text-ColorAgape"
+                >
+                    12.500
+                </div>
+                <div
+                    className="absolute top-[400px] left-[700px]
+                     cursor-pointer rounded-full border-2 border-ColorAgape
+                     backdrop-blur px-[16px] py-[8px]
+                     font-bold text-2xl text-ColorAgape
+                     text-center whitespace-nowrap"
+                >
+                    Ordena ahora
                 </div>
             </div>
         </section>
-    )
-}
-export default HeroAgape
+    );
+};
+export default HeroAgape;
