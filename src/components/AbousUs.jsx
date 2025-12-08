dc22c88c789f92007238805e2e1f60991777d87e
Mucca03 <alejito4d@gmail.com>
2025-12-08 18:53:11 -0500

    Add responsive styles

--- src/components/AbousUs.jsx
+++ src/components/AbousUs.jsx
@@ -1,32 +1,48 @@
-import {flowersHeader, selloAgape, tortaAgapeA} from "../assets/index.js"
+import {selloAgape, tortaAgapeA} from "../assets/index.js"
 
 const AboutUs = () => {
     return (
-
         <section>
             <div className="relative bg-GreenAU font-poppins">
-                <div className="bg-agapeHeader">
-                    <svg className="absolute -top-20" viewBox="0 0 1440 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <div className="relative">
+                    <div className="bg-GreenAU"></div>
+                    <svg
+                        className="absolute"
+                        viewBox="0 0 1440 100"
+                        xmlns="http://www.w3.org/2000/svg">
                         <path
-                            d="M0 43.9999C106.667 43.9999 213.333 7.99994 320 7.99994C426.667 7.99994 533.333 43.9999 640 43.9999C746.667 43.9999 853.333 7.99994 960 7.99994C1066.67 7.99994 1173.33 43.9999 1280 43.9999C1386.67 43.9999 1440 19.0266 1440 9.01329V100H0V43.9999Z"
-                            className="fill-GreenAU"></path>
+                            fill="currentColor"
+                            className="text-agapeHeader"
+                            fillRule="evenodd"
+                            d="M0 0H1440V100H0V0Z M0 43.9999 C106.667 43.9999 213.333 7.99994 320 7.99994 C426.667 7.99994 533.333 43.9999 640 43.9999 C746.667 43.9999 853.333 7.99994 960 7.99994 C1066.67 7.99994 1173.33 43.9999 1280 43.9999 C1386.67 43.9999 1440 19.0266 1440 9.01329V100H0V43.9999Z"/>
                     </svg>
                 </div>
-                <div className="absolute right-[0px] z-10 top-[0px]">
-                    <img src={tortaAgapeA} className="lg:w-[400px] 2xl:w-[620px]" alt="TortaAgape"/>
-                </div>
-                <div className="absolute 2xl:left-[1420px] lg:left-[1000px] z-10 lg:-top-15 2xl:top-[50px]">
-                    <img src={selloAgape} className="w-[300px] h-auto" alt="selloAgape" />
+                <div className="hidden lg:block absolute right-0 top-10 z-10">
+                    <div className="relative lg:w-[400px] 2xl:w-[620px]">
+                        <img
+                            src={tortaAgapeA}
+                            alt="TortaAgape"
+                            className="w-full h-auto"
+                        />
+                        <img
+                            src={selloAgape}
+                            alt="SelloAgape"
+                            className="absolute top-[10px] lg:left-[30px] 2xl:left-[150px]
+                            w-[210px] lg:w-[260px] 2xl:w-[310px]"/>
+                    </div>
                 </div>
 
-                <div className="grid 2xl:grid-cols-[150px_1fr_480px] lg:grid-cols-[150px_1fr_340px] gap-2">
+                <div className="grid 2xl:grid-cols-[150px_1fr_480px]
+                lg:grid-cols-[150px_1fr_340px] gap-3
+                px-5">
                     <div className=""></div>
-                    <div className="2xl:space-y-10 lg:space-y-8 mt-20">
-                        <h1 className="2xl:text-7xl lg:text-5xl font-bold text-agapeText">
+                    <div className="2xl:space-y-10 lg:space-y-8 lg:mt-20">
+                        <h1 className="text-2xl 2xl:text-7xl lg:text-5xl font-bold text-agapeText mt-20">
                             ¿Quienes somos?
                         </h1>
 
-                        <p className="2xl:text-5xl/15 lg:text-3xl/10 text-ColorAgape
+                        <p className="text-base/7
+                        md:text-lg/8 2xl:text-5xl/15 lg:text-3xl/10 text-ColorAgape
                         font-extralight italic">
                             En <b>Ágape</b>, somos más que una respostería <b>somos una
                             familia apasionada</b> por el arte de la reposteria
@@ -36,7 +52,7 @@ const AboutUs = () => {
                             contigo. Comenzamos nuestra aventura con unos
                             deliciosos cupcakes y <b>hoy tenemos mucho más para ti</b>
                         </p>
-                        <div className="space-x-10 mb-15">
+                        <div className="space-x-10 mb-5 mt-3">
                             <button
                                 className="bg-buttomAgape
                             cursor-pointer rounded-full
@@ -63,9 +79,6 @@ const AboutUs = () => {
                     </div>
                 </div>
                 <div className="w-full bg-gradient-to-b from-GreenAU to-agapeHeader h-[100px] 2xl:top-[800px] lg:top-[570px]"></div>
-                <div className="absolute -bottom-15 lg:left-1/2 lg:-translate-x-1/2">
-                    <img src={flowersHeader} className="2xl:h-[150px] lg:h-[120px]" alt="Flowers"/>
-                </div>
             </div>
         </section>
     )
--- src/components/HeroAgape.jsx
+++ src/components/HeroAgape.jsx
@@ -3,58 +3,54 @@ import {logoAgape, tortaAgape} from "../assets/index.js"
 const HeroAgape = () => {
     return (
         <section className="bg-agapeHeader py-15 mb-10">
-            <div className="relative max-w-screen-xl mx-auto h-[800px] font-poppins">
+            <div className="relative max-w-screen-xl mx-auto flex justify-center font-poppins">
                 <img
                     src={logoAgape}
-                    className="absolute z-10 left-1/2 -translate-x-1/2 w-[500px] h-auto"
+                    className="absolute z-10 left-1/2 -translate-x-1/2
+                     w-[50%] max-w-[500px] h-auto"
                     alt="logo"
                 />
                 <img
                     src={tortaAgape}
-                    className="
-                    absolute z-0 left-1/2 -translate-x-1/2
-                    w-[850px] h-auto
-                    "
+                    className="z-0
+                    w-[80%] max-w-[850px] h-auto"
                     alt="mockup"
                 />
-                <div
-                    className="
-                    absolute left-1/2 bottom-[0px]
-                    -translate-x-1/2
-                    w-[350px] h-[40px]
-                    bg-black/40
-                    rounded-full blur-2xl
-                    z-0"
-                ></div>
-                <div
-                    className="absolute top-[244px] left-[140px]
-                     text-4xl font-bold text-ColorAgape"
-                >
-                    Pastel Velvet
-                </div>
-                <div
-                    className="absolute top-[280px] left-[70px]
-                     text-2xl text-redText
-                     backdrop-blur rounded-full px-[20px] py-[12px]
-                     w-[450px] font-light italic
-                     border-1 border-gray-50"
-                >
-                    Bizcocho esponjoso de cacao con crema de queso suave y un sello rojo irresistible
-                </div>
-                <div
-                    className="absolute top-[515px] left-[870px]
-                     text-4xl font-bold text-ColorAgape"
-                >
-                    12.500
-                </div>
-                <div
-                    className="absolute top-[560px] left-[770px]
-                     cursor-pointer rounded-full border-2 border-ColorAgape
-                     backdrop-blur px-[25px] py-[15px]
-                     font-bold text-4xl text-ColorAgape
-                     text-center whitespace-nowrap"
-                >
-                    Ordena ahora
+
+                <div className="absolute inset-0 flex flex-col justify-center z-11">
+                    <div className="absolute left-[5%] top-[25%] max-w-[450px] w-[40%]">
+                        <h1 className="text-lg sm:text-3xl lg:text-4xl  font-bold text-ColorAgape">Pastel Velvet</h1>
+                        <p
+                            className="
+                            text-xs sm:text-base lg:text-2xl text-redText
+                            backdrop-blur rounded-full px-4 py-2
+                            font-light italic
+                            border-1 border-gray-50"
+                        >
+                            Bizcocho esponjoso de cacao con crema de queso suave y un sello rojo irresistible
+                        </p>
+                    </div>
+
+                    <div className="absolute right-[15%] bottom-[25%] flex flex-col items-end
+                        ">
+                        <h1
+                            className="text-2xl md:text-3xl lg:text-4xl
+                            mr-[30%] font-bold text-ColorAgape">
+                            12.500
+                        </h1>
+                        <button className="cursor-pointer rounded-full border-2 border-ColorAgape
+                        backdrop-blur font-bold text-base sm:text-xl md:text-3xl lg:text-4xl
+                        px-1 py-1
+                        sm:px-2 sm:py-2
+                        md:px-4 md:py-2
+                        lg:px-6 lg:py-3
+                        text-ColorAgape
+                        text-center whitespace-nowrap"
+                        >
+                            Ordena ahora
+                        </button>
+                    </div>
+
                 </div>
             </div>
         </section>
